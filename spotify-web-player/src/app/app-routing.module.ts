a06fa172db7b3c3614bd42897f40e8e1214a1b48
mGnadlinger <m.gnadlinger@students.htl-leonding.ac.at>
2025-12-22 11:00:54 +0000

    feature: play five placeholder songs, added control buttons #22

---
+++ spotify-web-player/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Player } from './pages/player/player';
+
+
+const routes: Routes = [
+  { path: '', component: Player }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
