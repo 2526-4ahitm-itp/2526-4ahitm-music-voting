1025fd6ef4d4744a2783be09d257af6044268e03
SimoneSperrer <s.sperrer@students.htl-leonding.ac.at>
2026-01-15 14:01:48 +0100

    fix: added swift app

---
+++ musicvoting/app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Sperrer Simone on 15.01.26.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musicvoting/app/appTests/appTests.swift
@@ -0,0 +1,17 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by Sperrer Simone on 15.01.26.
+//
+
+import Testing
+@testable import app
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
