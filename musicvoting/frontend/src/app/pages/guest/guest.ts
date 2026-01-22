5f17b5265e5882e0899b79d5a42a18697cf99a33
mGnadlinger <m.gnadlinger@students.htl-leonding.ac.at>
2026-01-22 13:32:46 +0000

    search for song as a guest

--- musicvoting/frontend/src/app/pages/guest/guest.ts
+++ musicvoting/frontend/src/app/pages/guest/guest.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpotifyWebPlayerService } from '../../services/spotify-player';
+import { TrackService } from '../../services/spotify-tracks';
 
 @Component({
   selector: 'app-guest',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './guest.html',
-  styleUrl: './guest.css',
 })
-export class Guest {
+export class Guest implements OnInit {
+  tracks: any[] = [];
 
+  constructor(
+    private spotifyService: SpotifyWebPlayerService,
+    private trackApi: TrackService
+  ) {}
+
+  async ngOnInit() {
+    await this.spotifyService.initPlayer();
+  }
+
+  search() {
+    const query = (document.getElementById("search-input") as HTMLInputElement).value;
+
+    console.log("Suche wird gestartet...");
+    this.trackApi.searchTracks(query).subscribe({
+      next: (res) => {
+        this.tracks = res.tracks.items;
+      },
+      error: (err) => {
+        console.error("Fehler bei der Suche:", err);
+      }
+    });
+  }
+
+  play(uri: string) {
+    this.spotifyService.playTrack(uri);
+  }
 }
