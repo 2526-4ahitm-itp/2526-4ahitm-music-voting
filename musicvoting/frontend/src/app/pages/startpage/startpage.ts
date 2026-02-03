851ab5fb3e91db490999b15e6de0e2843270d385
MarliesWkbr <m.winklbauer@students.htl-leonding.ac.at>
2026-02-03 09:52:12 +0100

    feat: add component startpage

---
+++ musicvoting/frontend/src/app/pages/startpage/startpage.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpotifyWebPlayerService } from '../../services/spotify-player';
+import {TrackService} from '../../services/spotify-tracks';
+
+@Component({
+  selector: 'app-startpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './startpage.html',
+  styleUrl: './startpage.css',
+})
+
+export class Startpage implements OnInit{
+  tracks: any[] = [];
+
+  constructor(
+    private spotifyService: SpotifyWebPlayerService,
+    private trackApi: TrackService
+  ) {}
+
+  async ngOnInit() {
+    await this.spotifyService.initPlayer();
+  }
+}
+
