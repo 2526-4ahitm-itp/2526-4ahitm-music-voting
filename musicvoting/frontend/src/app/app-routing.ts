5dc4607542b6bb3f721bc4f1985e936033808179
MarliesWkbr <m.winklbauer@students.htl-leonding.ac.at>
2026-02-05 15:29:09 +0100

    style: add html-template

--- musicvoting/frontend/src/app/app-routing.ts
+++ musicvoting/frontend/src/app/app-routing.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Host } from './pages/host/host';
 import {Home} from './pages/home/home';
 import {Guest} from './pages/guest/guest';
+import {Startpage} from './pages/startpage/startpage';
 
 export const routes: Routes = [
+  {path: 'startpage', component: Startpage},
   { path: 'host', component: Host },
   {path: 'guest', component: Guest},
   {path: '', component: Home}
