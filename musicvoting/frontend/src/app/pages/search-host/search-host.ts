93757130706e473e7d610006ca70a4273f496d43
SimoneSperrer <s.sperrer@students.htl-leonding.ac.at>
2026-04-23 11:09:32 +0200

    feature/style: search-host site, nav style

---
+++ musicvoting/frontend/src/app/pages/search-host/search-host.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpotifyWebPlayerService } from '../../services/spotify-player';
+import { TrackService } from '../../services/spotify-tracks';
+import { lastValueFrom } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import {Router, RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-search-host',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './search-host.html',
+  styleUrls: ['./search-host.css'],
+})
+export class SearchHost implements OnInit {
+  menuOpen = false;
+  tracks: any[] = [];
+  searchQuery: string = '';
+  isSearching = false;
+  addingTrackId: string | null = null;
+
+  constructor(
+    private trackApi: TrackService,
+    private spotifyService: SpotifyWebPlayerService,
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  /** Suche nach Tracks */
+  async search(query?: string) {
+    const searchTerm = query || this.searchQuery?.trim();
+    if (!searchTerm) return;
+
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur();
+    }
+
+    this.isSearching = true;
+    this.tracks = [];
+    this.cdr.detectChanges();
+
+    try {
+      const res: any = await lastValueFrom(this.trackApi.searchTracks(searchTerm));
+      console.log('Search Response:', res); // Debug
+
+      if (res?.tracks?.items?.length) {
+        const seen = new Set<string>();
+          this.tracks = res.tracks.items
+            .filter((track: any) => track?.id && !seen.has(track.id))
+            .map((track: any) => {
+              seen.add(track.id);
+              return track;
+            })
+            .slice(0, 25);
+
+      }
+    } catch (err) {
+      console.error('Fehler bei der Suche:', err);
+    } finally {
+      this.isSearching = false;
+      this.cdr.detectChanges();
+    }
+  }
+
+  /** Track zur Playlist hinzufügen */
+  async addToPlaylist(track: any) {
+    this.addingTrackId = track.id;
+    this.cdr.detectChanges();
+
+    try {
+      await lastValueFrom(this.spotifyService.addToPlaylist(track.uri));
+      console.log(`${track.name} wurde zur Playlist hinzugefügt`);
+    } catch (err) {
+      console.error('Fehler beim Hinzufügen:', err);
+    } finally {
+      this.addingTrackId = null;
+      this.cdr.detectChanges();
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
